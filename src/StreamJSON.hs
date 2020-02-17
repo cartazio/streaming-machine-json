b5991be08ceb55554d5acb8ae495ecf12b51612f
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2020-02-17 15:42:42 -0500

    conditionalize using GHC.Generics to base >= 4.4

--- src/StreamJSON.hs
+++ src/StreamJSON.hs
@@ -1,15 +1,21 @@
-{-# LANGUAGE DeriveFoldable,DeriveTraversable,DeriveGeneric, DeriveDataTypeable,DeriveFunctor #-}
+{-# LANGUAGE DeriveFoldable,DeriveTraversable,DeriveGeneric, DeriveDataTypeable,DeriveFunctor, CPP #-}
 module StreamJSON (SnocList (..), Accessor (..), PrimVal (..)) where
 import Data.Text
 import Data.Typeable
+#if MIN_VERSION_base(4,4,0)
 import GHC.Generics
+#endif
 import Data.Foldable
 import Data.Traversable
 
 data Accessor
   = ArrayIx Int
   | ObjectField Text
-  deriving (Eq,Ord,Show,Generic,Typeable)
+  deriving (Eq,Ord,Show,Typeable,
+#if MIN_VERSION_base(4,4,0)
+            Generic
+#endif
+    )
 
 data PrimVal
   = PrimString !Text
@@ -18,11 +24,19 @@ data PrimVal
   | PrimNull
   | PrimEmptyArray
   | PrimEmptyObject
-  deriving (Eq,Ord,Show,Generic,Typeable)
+  deriving (Eq,Ord,Show,Typeable,
+#if MIN_VERSION_base(4,4,0)
+            Generic
+#endif
+    )
 
 infixl  5 :|
 
 data SnocList a
   = RNil
   | (:|) (SnocList a) a
-  deriving (Eq,Ord,Functor,Foldable,Traversable,Show)
+  deriving (Eq,Ord,Functor,Foldable,Traversable,Show,
+#if MIN_VERSION_base(4,4,0)
+            Generic
+#endif
+    )
